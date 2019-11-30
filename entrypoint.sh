04d8275c04bb14e4163c1afb492e12d8fbfce2cb
Wan Yi <root@wanyi.dev>
2019-11-30 14:35:04 +0800

    增加删除老文件的功能

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DB_USER=${DB_USER:-${MYSQL_ENV_DB_USER}}
-DB_PASS=${DB_PASS:-${MYSQL_ENV_DB_PASS}}
-DB_NAME=${DB_NAME:-${MYSQL_ENV_DB_NAME}}
-DB_HOST=${DB_HOST:-${MYSQL_ENV_DB_HOST}}
+if [[ ${BACKUP_NUMBER} == "" ]]; then
+	echo "Missing BACKUP_NUMBER env variable"
+	exit 1
+fi
 
 if [[ ${DB_USER} == "" ]]; then
 	echo "Missing DB_USER env variable"
@@ -25,5 +25,21 @@ if [[ ${DB_HOST} == "" ]]; then
 	exit 1
 fi
 
+BACKUP_DIR=/var/db/backup
+TIMESTAMP=`date +%Y%m%d%H%M%S`
+
 echo "Dumping database: ${DB_NAME}"
-mysqldump -h ${DB_HOST} -u ${DB_USER} -p"${DB_PASS}" --triggers --routines --events ${DB_NAME} > ./mysqldump/${DB_NAME}_$(date +%F_%H%M%S).sql
+mysqldump -h ${DB_HOST} -u ${DB_USER} -p"${DB_PASS}" --triggers --routines --events ${DB_NAME} > ${BACKUP_DIR}/${DB_NAME}-${TIMESTAMP}.sql
+
+#写创建备份日志
+echo "create ${BACKUP_DIR}/${DB_NAME}-${TIMESTAMP}.sql" >> ${BACKUP_DIR}/log.txt
+#找出需要删除的备份
+delfile=`ls -l -crt ${BACKUP_DIR}/*.sql | awk '{print $9 }' | head -1`
+#判断现在的备份数量是否大于$number
+count=`ls -l -crt  ${BACKUP_DIR}/*.sql | awk '{print $9 }' | wc -l`
+if [ ${count} -gt ${BACKUP_NUMBER} ]
+then
+  rm $delfile  # 删除最早生成的备份只保留number数量的备份
+  #写删除文件日志
+  echo "delete $delfile" >> ${BACKUP_DIR}/log.txt
+fi
